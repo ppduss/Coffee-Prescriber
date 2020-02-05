daa61f8e02994d47b74554032fdfbbb95d22854f
Patrick <ppduss@gmail.com>
2020-02-04 18:41:16 -0600

    More tinkering

--- src/App.js
+++ src/App.js
@@ -1,16 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
+const App = () => {
+
+  const [currentComponent, setCurrentComponent] = useState('hello')
+
+  const HelloComponent = () => (
     <div className="App">
       <header className="App-header">
         <h1>Are you ready for coffee ?</h1>
+        <button onClick={() => setCurrentComponent('next')}>next!</button>
       </header>
     </div>
+  )
+
+  const NextComponent = () => (
+    <div className="App">
+      <header className="App-header">
+        <h1>Next One here</h1>
+        <button onClick={() => setCurrentComponent('counter')}>go back</button>
+      </header>
+    </div>
+  )
+
+  const ActiveComponent = () => {
+    switch (currentComponent) {
+      case "hello":
+        return <HelloComponent />
+      case "next":
+        return <NextComponent />
+      case "counter":
+        return <Counter initialCount={2} />
+      default:
+        break;
+    }
+  }
 
 
-  );
+  function Counter({ initialCount }) {
+    const [count, setCount] = useState(initialCount);
+    return (
+      <>
+        Count: {count}
+        <button onClick={() => setCount(initialCount)}>Reset</button>
+        <button onClick={() => setCount(x => x - 1)}>-</button>
+        <button onClick={() => setCount(x => x + 1)}>+</button>
+      </>
+    );
+  }
+
+
+  return (
+    <>
+      <Counter initialCount={30} />
+      <ActiveComponent />
+    </>
+  )
 }
 
+
 export default App;
+
+
---
+++ src/steam.js
@@ -0,0 +1,15 @@
+
+
+
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="69.964 142.01 208.618 33.542"
+	 enable-background="new 69.964 142.01 208.618 33.542" xml:space="preserve">
+<path id="steam1" class="steam" fill="none" stroke="#FFFFFF" stroke-width="0.75" stroke-linecap="round" stroke-miterlimit="10" d="M84.326,151.098
+	c0,0-1.245-1.177-1.245-2.387s2.011-2.814,2.011-3.94c0-1.126-1.035-2.386-1.035-2.386"/>
+<path id="steam2" class="steam" fill="none" stroke="#FFFFFF" stroke-width="0.75" stroke-linecap="round" stroke-miterlimit="10" d="M89.376,151.098
+	c0,0-1.244-1.177-1.244-2.387s2.01-2.814,2.01-3.94c0-1.126-1.035-2.386-1.035-2.386"/>
+<path id="steam3" class="steam" fill="none" stroke="#FFFFFF" stroke-width="0.75" stroke-linecap="round" stroke-miterlimit="10" d="M94.428,151.098
+	c0,0-1.244-1.177-1.244-2.387s2.01-2.814,2.01-3.94c0-1.126-1.035-2.386-1.035-2.386"/>
+</svg>
+
+
