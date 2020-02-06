88a475ffa64fbcf04c7e62d2e927c20ef13e0af9
Patrick <ppduss@gmail.com>
2020-02-06 13:57:46 -0600

    Even more basic build

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
-import './App.css';
-import InlineSVG from 'svg-inline-react';
+import React, { useState } from 'react'
+import './App.css'
+import InlineSVG from 'svg-inline-react'
+import StarRatings from './StarRating.jsx'
 
 import SmilingImg from './assets/Slightly_Smiling_Emoji.png'
 import UnhappyImg from './assets/Unhappy_Emoji.png'
@@ -8,11 +9,13 @@ import ExpressionlessImg from './assets/Expressionless_Emoji.png'
 import SleepingImg from './assets/Sleeping_Emoji.png'
 import ConfusedImg from './assets/Confused_Emoji.png'
 import StarImg from './assets/medium_star.png'
+import FlaviconImg from './assets/favicon2.ico'
 
 
 const tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
 const currentTime = new Date();
 console.log(currentTime);
+console.log(tz);
 
 const App = () => {
 
@@ -21,11 +24,20 @@ const App = () => {
   const IntroComponent = () => (
     <div className="App">
       <header className="App-header">
+        <link rel="shortcut icon" href={FlaviconImg} />
+        <link rel='icon' href={FlaviconImg} type='image/x-icon' />
         <h1>Are you ready for coffee ?</h1>
         <form>
-          <label for="time">What time is it ?</label>
+          {/* <label for="time">What time is it ?</label>
           <input type="time" class="time" name="time"
-            min="01:00" max="24:00" step="600" required />
+            min="01:00" max="24:00" step="600" required /> */}
+
+          <select name="time">
+            <option value="1"> Age </option>
+            <option value="2"> 0-12</option>
+            <option value="3">12-18 </option>
+            <option value="4">18+</option>
+          </select>
 
           <select name="age">
             <option value="child"> Age </option>
@@ -70,22 +82,21 @@ const App = () => {
         </select></div>
     </div>
 
-
   )
 
   const Cup1Component = () => (
     <div className="App">
       <header className="App-header">
-        <h1>Cup #1</h1>
+        <h1>CUP: 1</h1>
 
-        <p>How did that feel ?</p>
+        <p>How do you feel ?</p>
 
         <form>
-          <input type="radio" class="button" name="Sleepy" value="" type="image" src={SleepingImg} alt="Expressionless face" alt="sleepy" height="42" width="42" />
+          <input type="radio" class="button" name="Sleeping" value="" type="image" src={SleepingImg} alt="Expressionless face" alt="sleepy" height="42" width="42" />
           <input type="radio" class="button" name="Expressionless" value="2" type="image" src={ExpressionlessImg} alt="Expressionless face" height="42" width="42" />
           <input type="radio" class="button" name="Smiling" value="3" type="image" src={SmilingImg} alt="Expressionless face" height="42" width="42" />
-          <input type="radio" class="button" name="test" value="4" type="image" src={ConfusedImg} alt="Expressionless face" height="42" width="42" />
-          <input type="radio" class="button" name="test" value="5" type="image" src={UnhappyImg} alt="Expressionless face" height="42" width="42" />
+          <input type="radio" class="button" name="Confused" value="4" type="image" src={ConfusedImg} alt="Expressionless face" height="42" width="42" />
+          <input type="radio" class="button" name="Unhappy" value="5" type="image" src={UnhappyImg} alt="Expressionless face" height="42" width="42" />
         </form>
 
         <p>How productive were you ?</p>
@@ -99,26 +110,11 @@ const App = () => {
         </form>
 
 
-        <button onClick={() => setCurrentComponent('intro')}>Reset</button>
+        <button onClick={() => setCurrentComponent('counter')}>counter</button>
       </header >
 
     </div >
   )
-
-  const ActiveComponent = () => {
-    switch (currentComponent) {
-      case "intro":
-        return <IntroComponent />
-      case "next":
-        return <Cup1Component />
-      case "counter":
-        return <Counter initialCount={2} />
-      default:
-        break;
-    }
-  }
-
-
 
   function Counter({ initialCount }) {
     const [count, setCount] = useState(initialCount);
@@ -128,18 +124,32 @@ const App = () => {
         <button onClick={() => setCount(initialCount)}>Reset</button>
         <button onClick={() => setCount(x => x - 1)}>-</button>
         <button onClick={() => setCount(x => x + 1)}>+</button>
-        const tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  console.log(tz)
+
       </>
     );
   }
 
+  const Stars = () => {
+    return <StarRatings />
+  }
 
-  return (
-    <>
-      <ActiveComponent />
-    </>
-  )
+  const ActiveComponent = () => {
+    switch (currentComponent) {
+      case "intro":
+        return <IntroComponent />
+      case "next":
+        return <Cup1Component />
+      case "counter":
+        return <Counter initialCount={2} />
+      case "starrating":
+        return <StarRatings />
+      default:
+        break;
+    }
+  }
+
+
+  return <ActiveComponent />
 }
 
 
