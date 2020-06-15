2083343571787968c0a100f1d219e97644088cc8
Patrick <ppduss@gmail.com>
2020-06-15 15:34:33 -0400

    Updates

---
+++ precache-manifest.159467db0facca1e7acfe5e71b8a7e20.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "705ff31f940baf24223951194753b1c1",
+    "url": "/Coffee-Prescriber/index.html"
+  },
+  {
+    "revision": "28ab50ec69fff1641393",
+    "url": "/Coffee-Prescriber/static/css/main.ba47f1ca.chunk.css"
+  },
+  {
+    "revision": "679cb665acbbf46337dc",
+    "url": "/Coffee-Prescriber/static/js/2.8416df5f.chunk.js"
+  },
+  {
+    "revision": "5d6955cce47e196fa1637128c967264d",
+    "url": "/Coffee-Prescriber/static/js/2.8416df5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28ab50ec69fff1641393",
+    "url": "/Coffee-Prescriber/static/js/main.28a0d808.chunk.js"
+  },
+  {
+    "revision": "0b965f7bbf4753aeadb3",
+    "url": "/Coffee-Prescriber/static/js/runtime-main.4840daca.js"
+  },
+  {
+    "revision": "419a60ad1999d29469503ec060b8bf6e",
+    "url": "/Coffee-Prescriber/static/media/blushingFace.419a60ad.png"
+  },
+  {
+    "revision": "ca4a3c600e9c4064a02f61fea64f0c50",
+    "url": "/Coffee-Prescriber/static/media/coffee.ca4a3c60.png"
+  },
+  {
+    "revision": "8cbe5b96990386437fd489c5825f1251",
+    "url": "/Coffee-Prescriber/static/media/collisionFace.8cbe5b96.png"
+  },
+  {
+    "revision": "2a862f61cbfb9cfb40d36f3696805161",
+    "url": "/Coffee-Prescriber/static/media/confoundedFace.2a862f61.png"
+  },
+  {
+    "revision": "179bc7075948d870857a39e4c6f49320",
+    "url": "/Coffee-Prescriber/static/media/confusedFace.179bc707.png"
+  },
+  {
+    "revision": "e58f4820aa9608649b92c33cfbf12c98",
+    "url": "/Coffee-Prescriber/static/media/dizzyFace.e58f4820.png"
+  },
+  {
+    "revision": "433beadaedc5147c64b6cf4de43116d6",
+    "url": "/Coffee-Prescriber/static/media/explodingFace.433beada.png"
+  },
+  {
+    "revision": "ebd7ad1c1d587f893fe6c2bcd9aedc7c",
+    "url": "/Coffee-Prescriber/static/media/expressionlessFace.ebd7ad1c.png"
+  },
+  {
+    "revision": "6deb9ee831b67c3202908c15d2ed9e1d",
+    "url": "/Coffee-Prescriber/static/media/flushedFace.6deb9ee8.png"
+  },
+  {
+    "revision": "f576abde8fa050048b85d9ae6c563628",
+    "url": "/Coffee-Prescriber/static/media/grimacingFace.f576abde.png"
+  },
+  {
+    "revision": "486f1077b6728ce889f8b27caee37b8d",
+    "url": "/Coffee-Prescriber/static/media/mediumStar.486f1077.png"
+  },
+  {
+    "revision": "b951b73ef3dc1d658f9310bbe88dafa9",
+    "url": "/Coffee-Prescriber/static/media/neutralFace.b951b73e.png"
+  },
+  {
+    "revision": "aab6fd889f1b436e9d6652b633646a82",
+    "url": "/Coffee-Prescriber/static/media/partyFace.aab6fd88.png"
+  },
+  {
+    "revision": "8bdf88928eacaba3f54ad6e3fa69a07b",
+    "url": "/Coffee-Prescriber/static/media/perseveringFace.8bdf8892.png"
+  },
+  {
+    "revision": "faa7fd061c2723960f67a4ba935f08e7",
+    "url": "/Coffee-Prescriber/static/media/poisonedFace.faa7fd06.png"
+  },
+  {
+    "revision": "9246e08070b84c40381ee02f042d0370",
+    "url": "/Coffee-Prescriber/static/media/skullFace.9246e080.png"
+  },
+  {
+    "revision": "fe8e98288cfcd135ea658b5b8ce16153",
+    "url": "/Coffee-Prescriber/static/media/sleepingFace.fe8e9828.png"
+  },
+  {
+    "revision": "7436e382498e0e4df4740464c598a7b7",
+    "url": "/Coffee-Prescriber/static/media/slightlySmilingFace.7436e382.png"
+  },
+  {
+    "revision": "f0cd583f0a68b270d504ab94aeb9fd02",
+    "url": "/Coffee-Prescriber/static/media/smilingFace.f0cd583f.png"
+  },
+  {
+    "revision": "b14191bad0e3dff94bc0c6e17a9e9619",
+    "url": "/Coffee-Prescriber/static/media/starEyesFace.b14191ba.png"
+  },
+  {
+    "revision": "edb20bb8de846262349583a2aab978cf",
+    "url": "/Coffee-Prescriber/static/media/sunglassesFace.edb20bb8.png"
+  },
+  {
+    "revision": "6305febff9d2a75884c5fb14b84bc808",
+    "url": "/Coffee-Prescriber/static/media/tongueOutFace.6305febf.png"
+  },
+  {
+    "revision": "770be50d967d013348818c3ebd13d1e5",
+    "url": "/Coffee-Prescriber/static/media/unhappyFace.770be50d.png"
+  },
+  {
+    "revision": "375c22f2c69763f849cfcfb1ab44bad0",
+    "url": "/Coffee-Prescriber/static/media/verySadFace.375c22f2.png"
+  },
+  {
+    "revision": "8728e3a3549db827265c2ba84180b707",
+    "url": "/Coffee-Prescriber/static/media/wearyFace.8728e3a3.png"
+  }
+]);
